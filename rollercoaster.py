546ec0de96a37697be6200ee8d32bba2384b867c
Edward Boland <mr.eboland@gmail.com>
2021-01-20 16:13:09 -0500

    More input()

---
+++ rollercoaster.py
@@ -0,0 +1,8 @@
+# Using int() in an actual program
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print("\nYou're tall enough to ride")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
