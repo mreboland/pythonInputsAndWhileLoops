8cd33ee67178e49e5c36243218586f8a51128132
Edward Boland <mr.eboland@gmail.com>
2021-01-20 16:40:13 -0500

    intro to while loops

---
+++ counting.py
@@ -0,0 +1,12 @@
+# While loops
+# The for loop takes a collection of items and executes a block of code once for each item in the collection. A while loop runs as long as, or while, a certain condition is true.
+# An example of counting to 5:
+# We set a counter, currentNumber to 1 to have a start for our count
+currentNumber = 1
+
+# While currentNumber is less than or equal to 5 this will loop until it hits 5
+while currentNumber <= 5:
+    # Print the number
+    print(currentNumber)
+    # Increase currentNumber by 1 to increase the count. It's the same as currentNumber = currentNumber + 1
+    currentNumber += 1
