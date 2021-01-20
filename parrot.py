772120c8e438dbaacde197e762e1b117a6f7dd29
Edward Boland <mr.eboland@gmail.com>
2021-01-20 15:44:25 -0500

    learning input()

---
+++ parrot.py
@@ -0,0 +1,5 @@
+# How input() function works
+# The input function pauses your program and waits for the user to enter some text. Once received, the input is assigned to a variable which we can then use.
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
